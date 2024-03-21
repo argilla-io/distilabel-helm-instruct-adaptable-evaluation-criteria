ce1e1c87c5acfc6cf1a6ba8deae31cb52dbe2882
davidberenstein1957 <david.m.berenstein@gmail.com>
2024-03-21 23:34:12 +0100

    Rename `run_geitje` to `run_nl`

--- run_nl.py
+++ run_nl.py
@@ -5,6 +5,7 @@ from distilabel.dataset import DatasetCheckpoint
 from distilabel.llm import OpenAILLM
 from distilabel.pipeline import Pipeline
 
+from helm_instruct.criterion.base import Criterion, Rating
 from helm_instruct.criterion.nl import default_criterion
 from helm_instruct.evaluator.evaluator import HelmInstructTask
 from helm_instruct.evaluator.template.nl import template
@@ -15,7 +16,7 @@ NEW_DATASET_NAME = "davidberenstein1957/ultra_feedback_dutch_cleaned_helm_instru
 dataset = load_dataset("BramVanroy/ultra_feedback_dutch_cleaned")
 dataset = dataset.rename_column("prompt", "prompt_english")
 dataset = dataset.rename_column("prompt_dutch", "prompt")
-dataset = dataset["train"].select(range(10))
+dataset = dataset["train"].select(range(100))
 irrelevant_columns = ["prompt", "prompt_english", "prompt_dutch"]
 relevant_columns = [
     column_name
@@ -25,6 +26,32 @@ relevant_columns = [
 
 system_prompt_dutch = "Je bent een AI-responsbeoordelaar die zich richt op het beoordelen van instructies die duidelijk, interessant en complex zijn voor het verfijnen van open source LLM's."
 task_description_dutch = "Het volgende is een instructie geschreven door een mens en een reactie op de instructie geschreven door een AI-model. Beantwoord alstublieft de volgende vragen over de reactie van het AI-model."
+
+default_criterion["dutchness"] = Criterion(
+    question="Is de tekst in vloeiend en gramaticaal correct Nederlands geschreven?",
+    ratings=[
+        Rating(
+            value=1,
+            description="De tekst is niet vloeiend en bevat veel grammaticale fouten",
+        ),
+        Rating(
+            value=2,
+            description="De tekst is niet vloeiend en bevat enkele grammaticale fouten",
+        ),
+        Rating(
+            value=3,
+            description="De tekst is vloeiend en bevat enkele grammaticale fouten",
+        ),
+        Rating(
+            value=4,
+            description="De tekst is vloeiend en bevat nauwelijks grammaticale fouten",
+        ),
+        Rating(
+            value=5,
+            description="De tekst is vloeiend en bevat geen grammaticale fouten",
+        ),
+    ],
+)
 
 # phase2 - review responses
 checkpoint_strategy = DatasetCheckpoint(
@@ -37,8 +64,8 @@ checkpoint_strategy = DatasetCheckpoint(
     },
     save_frequency=1,
 )
-skip_dry_run = True
 for column_name in relevant_columns:
+    skip_dry_run = True
     dataset = dataset.rename_column(
         column_name, "response"
     )  # set column to correct input column
@@ -60,8 +87,7 @@ for column_name in relevant_columns:
         )
         dataset = pipe.generate(
             dataset,
-            num_generations=1,
-            batch_size=16,
+            batch_size=100,
             skip_dry_run=skip_dry_run,
             checkpoint_strategy=checkpoint_strategy,
         )
@@ -73,6 +99,10 @@ for column_name in relevant_columns:
         dataset = dataset.rename_column("rating", f"rating_{criterion_column}")
         dataset = dataset.rename_column("rationale", f"rationale_{criterion_column}")
         skip_dry_run = False
+        break
     # convert back to original column name to avoid losing data
     dataset = dataset.rename_column("response", column_name)
+    if len(dataset) > 100:
+        break
+    print(len(dataset))
 dataset.push_to_hub(NEW_DATASET_NAME, token=HF_API_TOKEN)
